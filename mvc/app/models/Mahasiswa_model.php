4fe689110d645e0e98b9ac0e5d29c5c03d8db65c
kelvin-203040062 <75973924+kelvin-203040062@users.noreply.github.com>
2022-10-12 23:51:38 +0700

    Update Mahasiswa_model.php

--- mvc/app/models/Mahasiswa_model.php
+++ mvc/app/models/Mahasiswa_model.php
@@ -12,7 +12,7 @@ class Mahasiswa_model {
 
 //    private $mhs = [
 //        [
-//            "nama" => "Kelvin Ardiansah",
+//            "nama" => "Kelvin ardiansah",
 //            "nrp" => "203040062",
 //            "email" => "kelvinardiansah16@gmail.com",
 //            "jurusan" => "Informatika"
